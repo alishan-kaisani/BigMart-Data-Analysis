f96cf3c6dadcb5bbcba8a8139beec5496ba72fd4
jpozin <joelpozin97@gmail.com>
2017-12-26 15:18:04 -0500

    added bar chart for establishment year vs. sales

--- bigmart.py
+++ bigmart.py
@@ -5,5 +5,10 @@ def bigmart():
 
 	data = pd.read_csv("train-clean.csv")
 
+# Bar chart for Item_Outlet_Sales vs. Outlet_Establihsment_Year
+
+print(set(df["Outlet_Establishment_Year"]))
+plt.bar(df["Outlet_Establishment_Year"], df["Item_Outlet_Sales"], align="center")
+
 if __name__ == "__main__":
 	bigmart()
