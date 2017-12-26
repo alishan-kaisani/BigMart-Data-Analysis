9fa81e47d5195e2f388a6cbca395462ad9de9ab0
jpozin <joelpozin97@gmail.com>
2017-12-26 15:53:57 -0500

    hfs

--- bigmart.py
+++ bigmart.py
@@ -1,3 +1,5 @@
+from statistics import mean
+
 def bigmart():
 	from random import randrange
 	import pandas as pd
@@ -11,6 +13,12 @@ print(set(df["Outlet_Establishment_Year"]))
 plt.bar(df["Outlet_Establishment_Year"], df["Item_Outlet_Sales"], align="center")
 
 print(set(df[df["Outlet_Establishment_Year"].isnull()]["Outlet_Identifier"].values))
+
+# Find the mean of Item_Weight for each Item_Type
+type_weight_dict = {}
+for itemtype in set(df["Item_Type"]):
+	type_weight_dict[itemtype] = mean([x for x in df["Item_Weight"] if ])
+print(type_weight_dict)
 
 if __name__ == "__main__":
 	bigmart()
