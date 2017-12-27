02c2346e781378f95537621975da03e95c7824c3
Alishan Kaisani <786.alishan@gmail.com>
2017-12-27 15:36:57 -0500

    Model Completed and Submitted

--- bigmart.py
+++ bigmart.py
@@ -1,5 +1,3 @@
-from statistics import mean
-
 def bigmart():
 	from random import randrange
 	import pandas as pd
@@ -7,13 +5,13 @@ def bigmart():
 
 	data = pd.read_csv("train-clean.csv")
 
-	print(data[data["Item_Type"] = "Starchy_Foods"])
+	print(data[data["Item_Type"] == "Starchy_Foods"])
 	#This will give you all rows where the type is starchy foods
 
 # Find the mean of Item_Weight for each Item_Type
 type_weight_dict = {}
 for itemtype in set(df["Item_Type"]):
-	item_df = data[data["Item_Type"] = itemtype]
+	item_df = df[df["Item_Type"] == itemtype]
 	type_weight_dict[itemtype] = item_df["Item_Weight"].mean()
 print(type_weight_dict)
 
